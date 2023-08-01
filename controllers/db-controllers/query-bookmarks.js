4b93437f699e8ab57fd0ec47d4bb0a46cbd17761
Ryan <ryanpvoong@gmail.com>
2023-08-01 11:07:07 -0400

    fixed user query issue with correct user uid

--- controllers/db-controllers/query-bookmarks.js
+++ controllers/db-controllers/query-bookmarks.js
@@ -3,7 +3,7 @@ const { Bookmark } = require("../../db/db-conn.js");
 const queryBookmarks = async (req, res) => {
   try {
     // Non-case sensitive text search
-    const query = { '$text': { '$search' : `\"${req.params.searchItem}\"` }, userId: "bookmarkUser" };
+    const query = { '$text': { '$search' : `\"${req.params.searchItem}\"` }, userId: req.session.uid };
     const projection = { categoryName: 1, pathName: 1, title: 1, link_title: 1 };
     const searchBookmarks = await Bookmark.find(query, projection);
 
